50867c0f2fad44e39d8ddf04d0534feaf6f0316b
k.shuvo360 <k.shuvo360@gmail.com>
2022-07-20 16:01:08 +0600

    Edvive_Task1_Subha(final)

--- lib/Speachtotranslate.dart
+++ lib/Speachtotranslate.dart
@@ -1,4 +1,4 @@
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
